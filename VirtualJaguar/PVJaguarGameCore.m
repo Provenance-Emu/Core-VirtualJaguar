32a923c5b5f47ceb1097190bf44fd2d03c6ca124
Joe Mattiello <mail@joemattiello.com>
2018-03-28 04:34:35 -0400

    Remove unused libretro calls for saves

--- VirtualJaguar/PVJaguarGameCore.m
+++ VirtualJaguar/PVJaguarGameCore.m
@@ -323,38 +323,38 @@ __weak static PVJaguarGameCore *_current;
 
 - (void)loadSaveFile:(NSString *)path forType:(int)type
 {
-    size_t size = retro_get_memory_size(type);
-    void *ramData = retro_get_memory_data(type);
-    
-    if (size == 0 || !ramData)
-    {
-        return;
-    }
-    
-    NSData *data = [NSData dataWithContentsOfFile:path];
-    if (!data || ![data length])
-    {
-        DLog(@"Couldn't load save file.");
-    }
-    
-    [data getBytes:ramData length:size];
+//    size_t size = retro_get_memory_size(type);
+//    void *ramData = retro_get_memory_data(type);
+//
+//    if (size == 0 || !ramData)
+//    {
+//        return;
+//    }
+//
+//    NSData *data = [NSData dataWithContentsOfFile:path];
+//    if (!data || ![data length])
+//    {
+//        DLog(@"Couldn't load save file.");
+//    }
+//
+//    [data getBytes:ramData length:size];
 }
 
 - (void)writeSaveFile:(NSString *)path forType:(int)type
 {
-    size_t size = retro_get_memory_size(type);
-    void *ramData = retro_get_memory_data(type);
-    
-    if (ramData && (size > 0))
-    {
-        retro_serialize(ramData, size);
-        NSData *data = [NSData dataWithBytes:ramData length:size];
-        BOOL success = [data writeToFile:path atomically:YES];
-        if (!success)
-        {
-            DLog(@"Error writing save file");
-        }
-    }
+//    size_t size = retro_get_memory_size(type);
+//    void *ramData = retro_get_memory_data(type);
+//
+//    if (ramData && (size > 0))
+//    {
+//        retro_serialize(ramData, size);
+//        NSData *data = [NSData dataWithBytes:ramData length:size];
+//        BOOL success = [data writeToFile:path atomically:YES];
+//        if (!success)
+//        {
+//            DLog(@"Error writing save file");
+//        }
+//    }
 }
 
 - (BOOL)saveStateToFileAtPath:(NSString *)fileName

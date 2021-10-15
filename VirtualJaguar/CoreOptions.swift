c0e123982990e44326de329bacddaf3083366b24
Joseph Mattello <mail@joemattiello.com>
2021-10-15 01:50:17 -0400

    Options temp
    Signed-off-by: Joseph Mattello <mail@joemattiello.com>

--- VirtualJaguar/CoreOptions.swift
+++ VirtualJaguar/CoreOptions.swift
@@ -27,6 +27,7 @@ import PVSupport
 //		case grayscale
 //	}
 //
+
 extension PVJaguarGameCore: CoreOptional {
 	public static var options: [CoreOption] = {
 		var options = [CoreOption]()
@@ -34,8 +35,9 @@ extension PVJaguarGameCore: CoreOptional {
 		let biosGroup = CoreOption.group(display: CoreOptionValueDisplay(title: "BIOS", description: nil),
 										 subOptions: [biosOption])
         
-        let hacksGroup = CoreOption.group(display: CoreOptionValueDisplay(title: "Hacks", description: "Performance hacks that work with some games better than others."),
-                                         subOptions: [blitterOption, doomResHackOption, forcePalOption])
+        let hacksGroup = CoreOption.group(display: CoreOptionValueDisplay(title: "Hacks",
+                                                                          description: "Performance hacks that work with some games better than others."),
+                                         subOptions: [blitterOption, doomResHackOption, forcePalOption, mutliThreadedRenderingOption, doubleBufferOption])
 
 		options.append(biosGroup)
         options.append(hacksGroup)
@@ -63,30 +65,54 @@ extension PVJaguarGameCore: CoreOptional {
         let forcePalOption = CoreOption.bool(display: .init(title: "Force PAL", description: "Force PAL mode over NTSC. May fix ROMs that are misdetected or coded for NTSC.", requiresRestart: true), defaultValue: false)
         return forcePalOption
     }()
+    
+    static var mutliThreadedRenderingOption: CoreOption = {
+        let mutliThreadedRenderingOption = CoreOption.bool(
+            display: .init(
+                title: "Mutli-threaded Rendering",
+                description: "Render audio/video in seperate threads. May or may not improve performance.",
+                requiresRestart: true),
+            defaultValue: false)
+        return mutliThreadedRenderingOption
+    }()
+    
+    static var doubleBufferOption: CoreOption = {
+        let doubleBufferOption = CoreOption.bool(
+            display: .init(
+                title: "Double buffer Rendering",
+                description: "Render audio/video in a back buffer. May or may not improve peformance at a 1 frame delay cost.",
+                requiresRestart: true),
+            defaultValue: true)
+        return doubleBufferOption
+    }()
 }
 
-@objc extension PVJaguarGameCore {
-	public func setUSEBios(_ useBios: Bool) {
-		
-	}
+@objc public extension PVJaguarGameCore {
+    @objc var virtualjaguar_bios: Bool { PVJaguarGameCore.valueForOption(PVJaguarGameCore.biosOption).asBool }
+    @objc var virtualjaguar_usefastblitter: Bool { PVJaguarGameCore.valueForOption(PVJaguarGameCore.blitterOption).asBool }
+    @objc var virtualjaguar_doom_res_hack: Bool { PVJaguarGameCore.valueForOption(PVJaguarGameCore.doomResHackOption).asBool }
+    @objc var virtualjaguar_pal: Bool { PVJaguarGameCore.valueForOption(PVJaguarGameCore.forcePalOption).asBool }
+    
+    @objc var virtualjaguar_mutlithreaded: Bool { PVJaguarGameCore.valueForOption(PVJaguarGameCore.mutliThreadedRenderingOption).asBool }
+    @objc var virtualjaguar_double_buffer: Bool {
+        get {
+            PVJaguarGameCore.valueForOption(PVJaguarGameCore.doubleBufferOption).asBool
+        }
+        set {
+            PVJaguarGameCore.setValue(newValue, forOption: PVJaguarGameCore.doubleBufferOption)
+        }
+    }
 }
 
-@objc extension PVJaguarGameCore {
-    @objc var virtualjaguar_bios: Bool { PVJaguarGameCore.valueForOption(Bool.self, "Jaguar BIOS") ?? false }
-    @objc var virtualjaguar_usefastblitter: Bool { PVJaguarGameCore.valueForOption(Bool.self, "Use Fast Blitter") ?? false }
-    @objc var virtualjaguar_doom_res_hack: Bool { PVJaguarGameCore.valueForOption(Bool.self, "DOOM Res Hack") ?? false }
-    @objc var virtualjaguar_pal: Bool { PVJaguarGameCore.valueForOption(Bool.self, "Force PAL") ?? false }
-}
-
-
-extension PVJaguarGameCore: CoreActions {
-	public var coreActions: [CoreAction]? {
-		let bios = CoreAction(title: "Use Jaguar BIOS", options: nil)
-		let fastBlitter =  CoreAction(title: "Use fast blitter", options:nil)
-		return [bios, fastBlitter]
-	}
-
-	public func selected(action: CoreAction) {
-		DLOG("\(action.title), \(String(describing: action.options))")
-	}
-}
+//
+//extension PVJaguarGameCore: CoreActions {
+//	public var coreActions: [CoreAction]? {
+//		let bios = CoreAction(title: "Use Jaguar BIOS", options: nil)
+//		let fastBlitter =  CoreAction(title: "Use fast blitter", options:nil)
+//		return [bios, fastBlitter]
+//	}
+//
+//	public func selected(action: CoreAction) {
+//		DLOG("\(action.title), \(String(describing: action.options))")
+//	}
+//}
